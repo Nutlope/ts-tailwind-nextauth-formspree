849cc5aace6b15761b65edc1dc2d177d870e69cd
nutlope <he55@drexel.edu>
2022-03-23 01:25:18 -0400

    added formspree

--- pages/form.tsx
+++ pages/form.tsx
@@ -1,97 +1,57 @@
 export default function Form() {
+  let form_id = ''
+  let formEndpoint = `https://formspree.io/f/${form_id}`
   return (
-    <form className="relative w-full max-w-lg">
-      <div className="absolute top-1/2 left-1/2 -mx-3 mb-6 flex h-20 w-20 flex-wrap">
-        <div className="mb-6 w-full px-3 md:mb-0 md:w-1/2">
-          <label className="mb-2 block text-xs font-bold uppercase tracking-wide text-gray-700">
-            First Name
-          </label>
+    <form
+      action={formEndpoint}
+      method="post"
+      className="w-30 mt-20 flex flex-col items-center"
+    >
+      <div className="mb-6">
+        <label className="mb-2 block text-sm font-medium dark:text-gray-900">
+          Your email
+        </label>
+        <input
+          type="email"
+          id="email"
+          className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+          placeholder="name@flowbite.com"
+          required
+        />
+      </div>
+      <div className="mb-6">
+        <label className="mb-2 block text-sm font-medium dark:text-gray-900">
+          Your password
+        </label>
+        <input
+          type="password"
+          id="password"
+          className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+          required
+        />
+      </div>
+      <div className="mb-6 flex items-start">
+        <div className="flex h-5 items-center">
           <input
-            className="mb-3 block w-full appearance-none rounded border border-red-500 bg-gray-200 py-3 px-4 leading-tight text-gray-700 focus:bg-white focus:outline-none"
-            id="grid-first-name"
-            type="text"
-            placeholder="Jane"
+            id="remember"
+            aria-describedby="remember"
+            type="checkbox"
+            className="focus:ring-3 h-4 w-4 rounded border border-gray-300 bg-gray-50 focus:ring-blue-300 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600"
+            required
           />
-          <p className="text-xs italic text-red-500">
-            Please fill out this field.
-          </p>
         </div>
-        <div className="w-full px-3 md:w-1/2">
-          <label className="mb-2 block text-xs font-bold uppercase tracking-wide text-gray-700">
-            Last Name
+        <div className="ml-3 text-sm">
+          <label className="font-medium text-gray-900 dark:text-gray-900">
+            Remember me
           </label>
-          <input
-            className="block w-full appearance-none rounded border border-gray-200 bg-gray-200 py-3 px-4 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
-            id="grid-last-name"
-            type="text"
-            placeholder="Doe"
-          />
         </div>
       </div>
-      <div className="-mx-3 mb-6 flex flex-wrap">
-        <div className="w-full px-3">
-          <label className="mb-2 block text-xs font-bold uppercase tracking-wide text-gray-700">
-            Password
-          </label>
-          <input
-            className="mb-3 block w-full appearance-none rounded border border-gray-200 bg-gray-200 py-3 px-4 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
-            id="grid-password"
-            type="password"
-            placeholder="******************"
-          />
-          <p className="text-xs italic text-gray-600">
-            Make it as long and as crazy as you'd like
-          </p>
-        </div>
-      </div>
-      <div className="-mx-3 mb-2 flex flex-wrap">
-        <div className="mb-6 w-full px-3 md:mb-0 md:w-1/3">
-          <label className="mb-2 block text-xs font-bold uppercase tracking-wide text-gray-700">
-            City
-          </label>
-          <input
-            className="block w-full appearance-none rounded border border-gray-200 bg-gray-200 py-3 px-4 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
-            id="grid-city"
-            type="text"
-            placeholder="Albuquerque"
-          />
-        </div>
-        <div className="mb-6 w-full px-3 md:mb-0 md:w-1/3">
-          <label className="mb-2 block text-xs font-bold uppercase tracking-wide text-gray-700">
-            State
-          </label>
-          <div className="relative">
-            <select
-              className="block w-full appearance-none rounded border border-gray-200 bg-gray-200 py-3 px-4 pr-8 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
-              id="grid-state"
-            >
-              <option>New Mexico</option>
-              <option>Missouri</option>
-              <option>Texas</option>
-            </select>
-            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-              <svg
-                className="h-4 w-4 fill-current"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-              >
-                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-              </svg>
-            </div>
-          </div>
-        </div>
-        <div className="mb-6 w-full px-3 md:mb-0 md:w-1/3">
-          <label className="mb-2 block text-xs font-bold uppercase tracking-wide text-gray-700">
-            Zip
-          </label>
-          <input
-            className="block w-full appearance-none rounded border border-gray-200 bg-gray-200 py-3 px-4 leading-tight text-gray-700 focus:border-gray-500 focus:bg-white focus:outline-none"
-            id="grid-zip"
-            type="text"
-            placeholder="90210"
-          />
-        </div>
-      </div>
+      <button
+        type="submit"
+        className="w-full rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:w-auto"
+      >
+        Submit
+      </button>
     </form>
   )
 }
